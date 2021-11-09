e1fd5d08433fc32fdb76073949bc08c676ab6bae
mrarmin1992 <52534979+mrarmin1992@users.noreply.github.com>
2021-11-09 14:13:31 +0100

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAyMs6ixXw0Ct8FYMycyoeuUS23ShwQdok',
+  authDomain: 'tvc-blog.firebaseapp.com',
+  projectId: 'tvc-blog',
+  storageBucket: 'tvc-blog.appspot.com',
+  messagingSenderId: '591307879753',
+  appId: '1:591307879753:web:f08630dac52850a2ed9698',
+  measurementId: 'G-B59S8EK0LG',
+  databaseURL: 'https://tvc-blog.firebaseio.com',
+  }
+};
